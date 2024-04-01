9e65c9a18521cdd1c1c2636605e1dbe739140223
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-04-01 17:24:09 +0330

    Add -> Update -> Create dto file to validate auth data And use swagger consumes in auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { AuthDto } from './dto/auth.dto';
+import { SwaggerConsumes } from 'src/common/enums';
 
 @Controller('auth')
 @ApiTags('Auth')
@@ -9,7 +10,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('user-existence')
-  @ApiConsumes('application/x-www-form-urlencoded')
+  @ApiConsumes(SwaggerConsumes.UrlEncoded,SwaggerConsumes.Json)
   async userExistenceC(@Body() auhtDto : AuthDto){
     return await this.authService.userExistenceS(auhtDto)
 
---
+++ src/modules/auth/dto/auth.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { AuthType } from 'src/common/enums';
+import { AuthMethods } from '../../../common/enums/auth/method.enum';
+import { IsEnum, IsString, MinLength } from 'class-validator';
+
+export class AuthDto {
+  @ApiProperty()
+  @IsString()
+  @MinLength(3)
+  username: string;
+  @ApiProperty({ enum: AuthType })
+  @IsEnum(AuthType)
+  type: string;
+  @ApiProperty({ enum: AuthMethods })
+  @IsEnum(AuthMethods)
+  method: string;
+}
