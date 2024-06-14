51562238fe0b88ee145aeb0db3e8261e85d8820e
AbolfazlGhaderi <brianprogrammer0@gmail.com>
2024-06-14 20:25:21 +0330

    Feat(Blog):  add  `/` in Controller
    this EndPoint added to receive All Blogs without Authentication

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -1,35 +1,41 @@
-import { Body, Controller, Post, UseGuards, HttpStatus, HttpCode, Get, UseInterceptors, HttpException } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, HttpStatus, HttpCode, Get, UseInterceptors, Query } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { CreateBlogDto } from './dto/create.blog.dto';
-import { ApiBearerAuth, ApiConsumes } from '@nestjs/swagger';
-import { SwaggerConfig } from 'src/configs';
+import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { PublicMessage, SwaggerConsumes } from 'src/common/enums';
 import { AuthGuard } from 'src/app/guards/auth.guard';
-import { get } from 'node:http';
 import { ResponseControllerInterceptor } from 'src/app/interceptors/response.controller.interceptor';
+import { Pagination } from 'src/common/decorators';
+import { PaginationDto } from 'src/common/dtos';
+import { SkipAuthDecorator } from 'src/common/decorators/skipAuth.decorator';
 
 @Controller('blog')
+@ApiTags(`Blog`)
+@UseGuards(AuthGuard)
+@ApiBearerAuth('Authorization')
 @UseInterceptors(ResponseControllerInterceptor)
 export class BlogController {
-  constructor(private readonly blogService: BlogService) {}
+   constructor(private readonly blogService: BlogService) {}
 
+   @Post('/new-blog')
+   @ApiConsumes(SwaggerConsumes.MultipartData, SwaggerConsumes.Json, SwaggerConsumes.UrlEncoded)
+   @HttpCode(HttpStatus.OK)
+   async CreateBlogC(@Body() blogData: CreateBlogDto): Promise<{ message: PublicMessage.CreateSuccess }> {
+      return await this.blogService.CreateBlogS(blogData);
+   }
 
-  @Post('/new-blog')
-  @ApiConsumes(SwaggerConsumes.MultipartData,SwaggerConsumes.Json,SwaggerConsumes.UrlEncoded)
-  @UseGuards(AuthGuard)
-  @ApiBearerAuth('Authorization')
-  @HttpCode(HttpStatus.OK)
-  async CreateBlogC(@Body() blogData:CreateBlogDto): Promise<{ message: PublicMessage.CreateSuccess }>{
+   @Get('/')
+   @HttpCode(HttpStatus.OK)
+   @Pagination()
+   @SkipAuthDecorator()
+   async BlogList(@Query() paginationData: PaginationDto) {
+      return await this.blogService.BlogList(paginationData);
+   }
 
-    return await this.blogService.CreateBlogS(blogData)
-  }
-
-  @Get('/myblogs')
-  @UseGuards(AuthGuard)
-  @ApiBearerAuth('Authorization')
-  @HttpCode(HttpStatus.OK)
-  async myBlogs(){
-    return await this.blogService.myBlog()
-    // throw new HttpException("this is test",HttpStatus.NOT_FOUND)
-  }
+   @Get('/myblogs')
+   @HttpCode(HttpStatus.OK)
+   async myBlogs() {
+      return await this.blogService.MyBlogs();
+      // throw new HttpException("this is test",HttpStatus.NOT_FOUND)
+   }
 }
