b38716dc6f3eeb96e118636f58b5f39659e18303
AbolfazlGhaderi <brianprogrammer0@gmail.com>
2024-06-17 19:11:04 +0330

    Feat(َUploade Interceptor): to add settings related to image storage

---
+++ src/app/interceptors/uploade.interceptor.ts
@@ -0,0 +1,8 @@
+import { MulterStorage } from '../utils/multer.util';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+export function UploadeImageInterceptor(fieldName: string, folderName: string = 'images') {
+   return class UploadeUtility extends FileInterceptor(fieldName, {
+      storage: MulterStorage(folderName),
+   }) {};
+}
