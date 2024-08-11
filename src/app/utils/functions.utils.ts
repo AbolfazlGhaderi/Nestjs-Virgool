99193dd178edf795c70d1d47c78d10f740663024
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-08-11 19:12:37 +0330

    Feat: add GenerateOtpKey and GenerateOtpSubject methods

--- src/app/utils/functions.utils.ts
+++ src/app/utils/functions.utils.ts
@@ -1,5 +1,7 @@
-import { randomBytes } from 'node:crypto';
 import { extname } from 'node:path';
+import { randomBytes } from 'node:crypto';
+import { OtpKey } from '../../common/enums/otp.keys.enum';
+import { CheckOtpMethods } from '../../modules/user/enums/enums';
 
 export const createSlug = (string: string) =>
 {
@@ -16,4 +18,40 @@ export function GenerateImageName(origialName: string, section:string)
     const name = origialName.split(extname(origialName))[0];
     const time = Date.now();
     return `${name}_${section}_${GenerateRandomByte(6)}_${time}${extname(origialName)}`;
+}
+
+export function GenerateOtpKey(method:CheckOtpMethods, conetnt:string)
+{
+    switch (method)
+    {
+        case CheckOtpMethods.Add: {
+            return `${conetnt}${OtpKey.Add}`;
+        }
+        case CheckOtpMethods.Change: {
+            return `${conetnt}${OtpKey.Change}`;
+        }
+        default: {
+            return `${conetnt}${OtpKey.Verify}`;
+        }
+    }
+
+}
+export function GenerateOtpSubject(otpKey:OtpKey)
+{
+    switch (otpKey)
+    {
+        case OtpKey.Add: {
+            return 'Add';
+        }
+        case OtpKey.Change: {
+            return 'Change';
+        }
+        case OtpKey.Verify: {
+            return 'Verify';
+        }
+        default: {
+            return 'Login';
+        }
+    }
+
 }
